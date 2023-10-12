b5d5f724db1c92cf71b24a52d2e6d6f65be354a7
iFederico <124475515+legit111@users.noreply.github.com>
2023-10-12 00:13:18 -0300

    Delete contraseña.js

--- contraseña.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("formulario").addEventListener("submit", function(event) {
-  var password = document.getElementById("password").value;
-  var confirm_password = document.getElementById("confirm_password").value;
-
-  if (password !== confirm_password) {
-    alert('Las contraseñas no coinciden. Por favor, inténtalo de nuevo.');
-    event.preventDefault(); // Evita que el formulario se envíe
-  }
-});
